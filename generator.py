68d278c8931360b48b124a218feba32e616ba0f8
Daniel Matias Ferrer <xassbit@gmail.com>
2016-10-08 23:02:05 +0200

    made paths dependent on root directory

--- generator.py
+++ generator.py
@@ -2,11 +2,14 @@ import sys
 import CVGenerator
 
 possible_commands = {
-    'generate' : CVGenerator.Website('skills').create_website()
+    'generate': lambda: CVGenerator.Website('skills').create_website()
 }
 
 if __name__ == "__main__":
-    main_command = sys.argv[1]
-    if main_command not in possible_commands.keys():
-        print('Sorry, command not found.')
-    possible_commands[main_command]
+    try:
+        main_command = sys.argv[1]
+        if main_command not in possible_commands.keys():
+            print('Sorry, command not found.')
+        possible_commands[main_command]()
+    except IndexError:
+        print('You need to input a command.')
