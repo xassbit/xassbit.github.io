e76dbb42ffa5b5752388abb2ec99de8ebdc3f7b7
xassbit <xassbit@gmail.com>
2018-05-05 11:59:42 +0200

    Add "Hello, World" to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,10 @@
+import "core-js/es6/map";
+import "core-js/es6/set";
+
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 import "./index.scss";
+
+ReactDOM.render(
+    <div>Hello, World</div>,
+    document.getElementById("content"));
