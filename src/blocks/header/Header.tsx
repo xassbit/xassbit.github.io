3fa7e568aa0a7893061bcd01ba3fc0881482f78d
xassbit <xassbit@gmail.com>
2018-05-05 17:40:33 +0200

    Add Header component

---
+++ src/blocks/header/Header.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import "./Header.scss";
+
+const Header: React.StatelessComponent = () =>
+    <header>
+        <div className="header-title">
+            <h1>Daniel Matias Ferrer</h1>
+            <h2>Curriculum Vitae</h2>
+        </div>
+    </header>;
+
+export default Header;
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,13 @@ import "core-js/es6/set";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import "./index.scss";
+import Header from "./blocks/header/Header";
+
+const App: React.StatelessComponent = () =>
+    <>
+        <Header/>
+    </>;
 
 ReactDOM.render(
-    <div>Hello, World</div>,
+    <App/>,
     document.getElementById("content"));
