8b34c8546dfaf712d6e7369bf179ef7891709625
xassbit <xassbit@gmail.com>
2018-05-06 14:18:31 +0200

    Style tabs with colours and spacing

--- src/elements/tabs/Tab.tsx
+++ src/elements/tabs/Tab.tsx
@@ -1,17 +1,33 @@
 import * as React from "react";
+import "./Tab.scss";
 
 interface TabProps {
     title: string;
+    open: boolean;
 }
 
-const Tab: React.StatelessComponent<TabProps> = ({title, children}) =>
-    <div className="tab">
-        <div className="tab-header">
-            <h3>{title}</h3>
-        </div>
-        <div className="tab-body">
-            {children}
-        </div>
-    </div>;
+interface TabState {
+    open: boolean;
+}
+
+class Tab extends React.Component<TabProps, TabState> {
+    constructor(props: TabProps) {
+        super(props);
+        this.state = {
+            open: this.props.open ? this.props.open : false,
+        };
+    }
+
+    public render() {
+        return <div className="tab">
+            <div className="tab-header">
+                <h3>{this.props.title}</h3>
+            </div>
+            <div className="tab-body">
+                {this.props.children}
+            </div>
+        </div>;
+    }
+}
 
 export default Tab;
