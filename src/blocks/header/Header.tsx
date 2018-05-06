5d3bdb62270378a4581ae9f248f87510db9c41d2
xassbit <xassbit@gmail.com>
2018-05-06 11:22:35 +0200

    Correct spacing on header

--- src/blocks/header/Header.tsx
+++ src/blocks/header/Header.tsx
@@ -4,12 +4,14 @@ import "./Header.scss";
 
 const Header: React.StatelessComponent = () =>
     <header>
-        <div className="header-image">
-            <img src={Headshot} alt="myheadshot"/>
-        </div>
-        <div className="header-title">
-            <h1>Daniel Matias Ferrer</h1>
-            <h2>Curriculum Vitae</h2>
+        <div className="header-wrapper">
+            <div className="header-image">
+                <img src={Headshot} alt="myheadshot"/>
+            </div>
+            <div className="header-title">
+                <h1>Daniel Matias Ferrer</h1>
+                <h2>Curriculum Vitae</h2>
+            </div>
         </div>
     </header>;
 
