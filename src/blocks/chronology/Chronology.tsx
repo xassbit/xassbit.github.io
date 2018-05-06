8d2de9cde71301869738ecd75694dd818ef38714
xassbit <xassbit@gmail.com>
2018-05-06 20:13:47 +0200

    Add element to style paragraphs

--- src/blocks/chronology/Chronology.tsx
+++ src/blocks/chronology/Chronology.tsx
@@ -1,74 +1,75 @@
 import * as React from "react";
 import Section from "../../elements/section/Section";
 import Tab from "../../elements/tab/Tab";
+import P from "../../elements/p/P";
 
 const Chronology: React.StatelessComponent = () =>
     <Section bottomPadding>
         <Tab title="Formative Years - 1991 to 2009">
-            <p>I was born in October 6th 1991 in Montemor-o-Novo, Portugal.</p>
-            <p>In 1996 I started classical percussion education
-                at the now extinct Academia de Música Eborense in Évora.</p>
-            <p>On my last year of high school I founded the music store A Baqueta
-                with the help from my family.</p>
-            <p>In 2009 I graduated from secondary school as the
-                highest graded student from the Sciences and Technologies course.</p>
+            <P>I was born in October 6th 1991 in Montemor-o-Novo, Portugal.</P>
+            <P>In 1996 I started classical percussion education
+                at the now extinct Academia de Música Eborense in Évora.</P>
+            <P>On my last year of high school I founded the music store A Baqueta
+                with the help from my family.</P>
+            <P important>In 2009 I graduated from secondary school as the
+                highest graded student from the Sciences and Technologies course.</P>
         </Tab>
         <Tab title="Physics in Porto - 2009 to 2012">
-            <p>In September of 2009 I moved from my hometown to study Physics at the University of Porto.</p>
-            <p>I became envolved with Physis, the Portuguese Association of Physics Students,
+            <P>In September of 2009 I moved from my hometown to study Physics at the University of Porto.</P>
+            <P>I became envolved with Physis, the Portuguese Association of Physics Students,
                 where I've served as president of the directive committee and the head organizer
-                of the 2011 and 2012 editions of the National Meeting of Physics Students.</p>
-            <p>In 2013 I completed the BSc in Physics.</p>
+                of the 2011 and 2012 editions of the National Meeting of Physics Students.</P>
+            <P>In 2013 I completed the BSc in Physics.</P>
         </Tab>
         <Tab title="Music in Porto - 2009 to 2012">
-            <p>Besides studying Physics, I've also also became a regular performer and music teacher in Porto.</p>
-            <p>Created stage performances as a solo artist for the now defunct Literary Club of Porto.</p>
-            <p>Joined sax player Paulo Alexandre Jorge to create the free jazz duo Bella Damião and
-                joined Paulo's music education project Carl Orff Projecto as a music teacher.</p>
-            <p>As Bella Damião we released a first album as a duo, played very reguarly
+            <P>Besides studying Physics, I've also also became a regular performer and music teacher in Porto.</P>
+            <P>Created stage performances as a solo artist for the now defunct Literary Club of Porto.</P>
+            <P>Joined sax player Paulo Alexandre Jorge to create the free jazz duo Bella Damião and
+                joined Paulo's music education project Carl Orff Projecto as a music teacher.</P>
+            <P>As Bella Damião we released a first album as a duo, played very reguarly
                 in Portugal and participated in the Free Jazz en Argentina festival
-                in Buenos Aires, where we recorded a second album with local musicians</p>
+                in Buenos Aires, where we recorded a second album with local musicians</P>
         </Tab>
         <Tab title="Physics in Nijmegen - 20012 to 2013">
-            <p>In 2013 I moved to the Netherlands for a one year stay at Radboud University Nijmegen
-                to study theoretical particle physics under the ERASMUS protocol.</p>
-            <p>During that time I've experimented with other venues of artistic production:
+            <P>In 2013 I moved to the Netherlands for a one year stay at Radboud University Nijmegen
+                to study theoretical particle physics under the ERASMUS protocol.</P>
+            <P>During that time I've experimented with other venues of artistic production:
                 started offering music production and graphic design services on Fiverr.com and
-                shot a web series documenting my life as an Erasmus student in the Netherlands.</p>
+                shot a web series documenting my life as an Erasmus student in the Netherlands.</P>
         </Tab>
         <Tab title="Solo Years - 2013 to 2016">
-            <p>In 2013 I started working more seriously in video production, having created the music
-                video Arritmia Aritmética for band A-nimal and co-produced the ICPS 2014 video diaries.</p>
-            <p>Worked as a freelancer under the company Controlled Flame and as a music teacher under
-                the company Arnhem Drums (later rebranded as Jam With Humans lessons)</p>
-            <p>Discovered the podcasting world, which lead to starting to learn task,
+            <P>In 2013 I started working more seriously in video production, having created the music
+                video Arritmia Aritmética for band A-nimal and co-produced the ICPS 2014 video diaries.</P>
+            <P>Worked as a freelancer under the company Controlled Flame and as a music teacher under
+                the company Arnhem Drums (later rebranded as Jam With Humans lessons)</P>
+            <P>Discovered the podcasting world, which lead to starting to learn task,
                 project and time management methodologies as well as exploring workflow
                 automation using iOS and Mac OS - which ultimately lead to starting to learn iOS
-                and later web development.</p>
+                and later web development.</P>
         </Tab>
         <Tab title="Consulting - 2016 to 2017">
-            <p>In May 2016 started to work for the consulting agency Newcraft, in the position of junior PHP
-                developer.</p>
-            <p>Later the same year briefly became the lead developer for the online store of one of the
-                largest telecom companies in the Netherlands</p>
-            <p>At the end of 2016 I was moved to the data science team, to help developing a
-                real-time marketing project in Scala, Apache Spark and Cassandra</p>
-            <p>Besides having worked for multiple clients with different technology needs and stacks,
-                gave internal lessons on Python and introduced the team to Django</p>
+            <P>In May 2016 started to work for the consulting agency Newcraft, in the position of junior PHP
+                developer.</P>
+            <P>Later the same year briefly became the lead developer for the online store of one of the
+                largest telecom companies in the Netherlands</P>
+            <P>At the end of 2016 I was moved to the data science team, to help developing a
+                real-time marketing project in Scala, Apache Spark and Cassandra</P>
+            <P>Besides having worked for multiple clients with different technology needs and stacks,
+                gave internal lessons on Python and introduced the team to Django</P>
         </Tab>
         <Tab title="In-house developer - from 2017">
-            <p>In May 2017 started to work for the tour operator srprs.me as a backend developer.</p>
-            <p>Slowly started to take the role of solutions architect, having migrated our projects
+            <P>In May 2017 started to work for the tour operator srprs.me as a backend developer.</P>
+            <P>Slowly started to take the role of solutions architect, having migrated our projects
                 to Webpack and introduced the team to React and Redux for the front-end,
                 and a REST framework for the frontend. A new website with this new tech stack was
-                deployed to production in April of 2018.</p>
+                deployed to production in April of 2018.</P>
         </Tab>
         <Tab title="Improvisational music - from 2014">
-            <p>From 2014 onwards I’ve focused only on not only playing but also teaching and
-                composing improvisational music.</p>
-            <p>Faded out individual music lessons and faded in workshops on improvisation.</p>
-            <p>CD “Not That Different”, released on March 2017, with Paulo Alexandre Jorge.</p>
-            <p>Presented the Jam With Humans Orchestra live in concert, starting in June 2017.</p>
+            <P>From 2014 onwards I’ve focused only on not only playing but also teaching and
+                composing improvisational music.</P>
+            <P>Faded out individual music lessons and faded in workshops on improvisation.</P>
+            <P>CD “Not That Different”, released on March 2017, with Paulo Alexandre Jorge.</P>
+            <P>Presented the Jam With Humans Orchestra live in concert, starting in June 2017.</P>
         </Tab>
     </Section>;
 
