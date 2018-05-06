20827236c636d6046ec25d2166f43b9521922f9f
xassbit <xassbit@gmail.com>
2018-05-06 14:43:44 +0200

    Remove uneeded component (Tab)

--- src/elements/tabs/Tabs.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-
-const Tabs: React.StatelessComponent = ({children}) =>
-    <div className="tabs-wrapper">
-        {children}
-    </div>;
-
-export default Tabs;
