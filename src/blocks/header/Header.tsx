792062ee9dc9acc7101451e6876a7ae835ecde15
xassbit <xassbit@gmail.com>
2018-05-06 11:16:55 +0200

    Add header image

--- src/blocks/header/Header.tsx
+++ src/blocks/header/Header.tsx
@@ -1,8 +1,12 @@
 import * as React from "react";
+import Headshot from "../../images/headshot.png";
 import "./Header.scss";
 
 const Header: React.StatelessComponent = () =>
     <header>
+        <div className="header-image">
+            <img src={Headshot} alt="myheadshot"/>
+        </div>
         <div className="header-title">
             <h1>Daniel Matias Ferrer</h1>
             <h2>Curriculum Vitae</h2>
