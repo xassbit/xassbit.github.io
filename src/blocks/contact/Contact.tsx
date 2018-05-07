10bb97418893658b00d3649836aecbad1955c8c6
xassbit <xassbit@gmail.com>
2018-05-07 21:52:27 +0200

    Add link to repo

--- src/blocks/contact/Contact.tsx
+++ src/blocks/contact/Contact.tsx
@@ -7,6 +7,8 @@ const Contact = () =>
         <P>You can see more about me at <a href="https://controlledflame.com">my website: controlledflame.com</a>.</P>
         <P>I can be contacted by email at controlledflame@gmail.com and by phone
             at +31 (0)6 11 47 56 48.</P>
+        <P>This website is hosted at GitHub Pages. You can find its source
+            code <a href="https://github.com/xassbit/xassbit.github.io">on GitHub</a>.</P>
     </Section>;
 
 export default Contact;
