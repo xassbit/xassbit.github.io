0eb2795aadf8b3afc1a4dbb83cfe93c946ddc3d6
xassbit <xassbit@gmail.com>
2018-05-07 08:37:30 +0200

    Update intro

--- src/blocks/intro/Intro.tsx
+++ src/blocks/intro/Intro.tsx
@@ -6,6 +6,7 @@ const Intro = () =>
         <p>Hi, I'm Daniel!</p>
         <p>I've studied and worked in several roles in the arts and sciences.
             Bellow you'll find a chronology of the most relevant stages of my life thus far.</p>
+        <p>I've left the parts most relevant to my carreer in development open.</p>
     </Section>;
 
 export default Intro;
