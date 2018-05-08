8b306989ad104cadeb12eb884bf501d21cf91873
xassbit <xassbit@gmail.com>
2018-05-08 17:44:17 +0200

    Format contacts

--- src/blocks/contact/Contact.tsx
+++ src/blocks/contact/Contact.tsx
@@ -4,9 +4,10 @@ import Section from "../../elements/section/Section";
 
 const Contact = () =>
     <Section bottomPadding>
-        <P>You can see more about me at <a href="https://controlledflame.com">my website: controlledflame.com</a>.</P>
-        <P>I can be contacted by email at controlledflame@gmail.com and by phone
-            at +31 (0)6 11 47 56 48.</P>
+        <P>You can see more about me at <a href="https://controlledflame.com">my website</a>.</P>
+        <P>I can be contacted by email
+            at <a href="mailto:controlledflame@gmail.com">controlledflame@gmail.com</a> and by phone
+            at +31&nbsp;(0)6&nbsp;11&nbsp;47&nbsp;56&nbsp;48.</P>
         <P>This website is hosted at GitHub Pages. You can find its source
             code <a href="https://github.com/xassbit/xassbit.github.io">on GitHub</a>.</P>
     </Section>;
