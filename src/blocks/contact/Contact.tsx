f3942a77df9a8462c2696b5a8b93319887f1771e
xassbit <xassbit@gmail.com>
2018-05-08 19:49:37 +0200

    Add link to LinkedIn

--- src/blocks/contact/Contact.tsx
+++ src/blocks/contact/Contact.tsx
@@ -8,6 +8,7 @@ const Contact = () =>
         <P>I can be contacted by email
             at <a href="mailto:controlledflame@gmail.com">controlledflame@gmail.com</a> and by phone
             at +31&nbsp;(0)6&nbsp;11&nbsp;47&nbsp;56&nbsp;48.</P>
+        <P>You can find my LinkedIn profile <a href="https://www.linkedin.com/in/danielmatiasferrer/">here</a>.</P>
         <P>This website is hosted at GitHub Pages. You can find its source
             code <a href="https://github.com/xassbit/xassbit.github.io">on GitHub</a>.</P>
     </Section>;
